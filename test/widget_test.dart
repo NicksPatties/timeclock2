836c5c3693938751606a96a265a2886883d64fae
NicksPatties <nickspatties@gmail.com>
2023-02-13 15:11:39 -0700

    First flutter codelab (#2)
    * Update dependencies
    
    * Clean up analysis options
    
    * Add idea word card component and button to change the word
    
    * Add favorites set to appState. Add button to like a word, and change its icon when its added to favorites.
    
    * Add navigation to favorites page. Make navigation component show menu labels when screen is 600 px wide
    
    * Create a favorites page that lists the current favorites that have been selected.
    
    * Update widget test and update package name to remove underscore

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -1,30 +1,24 @@
-// This is a basic Flutter widget test.
-//
-// To perform an interaction with a widget in your test, use the WidgetTester
-// utility in the flutter_test package. For example, you can send tap and scroll
-// gestures. You can also use WidgetTester to find child widgets in the widget
-// tree, read text, and verify that the values of widget properties are correct.
-
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_application_1/main.dart';
+import 'package:timeclock2/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Word favoriting smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(const MyApp());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the word has not been favorited
+    expect(find.byIcon(Icons.favorite_border), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the add favorites button
+    await tester.tap(find.byIcon(Icons.favorite_border));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify the word has been added to favorites
+    expect(find.byIcon(Icons.favorite_border), findsNothing);
+    // There are two widgets that have the favorite icon now, including the side menu
+    expect(find.byIcon(Icons.favorite), findsWidgets);
   });
 }
+
